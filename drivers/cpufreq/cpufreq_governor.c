afeb07e1e1b85d1625b31a106ef5347d9146bc3f
Viresh Kumar <viresh.kumar@linaro.org>
2014-06-09 14:21:24 +0530

    cpufreq: governor: remove copy_prev_load from 'struct cpu_dbs_common_info'
    'copy_prev_load' was recently added by commit: 18b46ab (cpufreq: governor: Be
    friendly towards latency-sensitive bursty workloads).
    
    It actually is a bit redundant as we also have 'prev_load' which can store any
    integer value and can be used instead of 'copy_prev_load' by setting it zero.
    
    True load can also turn out to be zero during long idle intervals (and hence the
    actual value of 'prev_load' and the overloaded value can clash). However this is
    not a problem because, if the true load was really zero in the previous
    interval, it makes sense to evaluate the load afresh for the current interval
    rather than copying the previous load.
    
    So, drop 'copy_prev_load' and use 'prev_load' instead.
    
    Update comments as well to make it more clear.
    
    There is another change here which was probably missed by Srivatsa during the
    last version of updates he made. The unlikely in the 'if' statement was covering
    only half of the condition and the whole line should actually come under it.
    
    Also checkpatch is made more silent as it was reporting this (--strict option):
    
    CHECK: Alignment should match open parenthesis
    +		if (unlikely(wall_time > (2 * sampling_rate) &&
    +						j_cdbs->prev_load)) {
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpufreq_governor.c
+++ drivers/cpufreq/cpufreq_governor.c
@@ -180,14 +180,19 @@ void dbs_check_cpu(struct dbs_data *dbs_data, int cpu)
 		 * - during long idle intervals
 		 * - explicitly set to zero
 		 */
-		if (unlikely(wall_time > (2 * sampling_rate)) &&
-						j_cdbs->copy_prev_load) {
+		if (unlikely(wall_time > (2 * sampling_rate) &&
+			     j_cdbs->prev_load)) {
 			load = j_cdbs->prev_load;
-			j_cdbs->copy_prev_load = false;
+
+			/*
+			 * Perform a destructive copy, to ensure that we copy
+			 * the previous load only once, upon the first wake-up
+			 * from idle.
+			 */
+			j_cdbs->prev_load = 0;
 		} else {
 			load = 100 * (wall_time - idle_time) / wall_time;
 			j_cdbs->prev_load = load;
-			j_cdbs->copy_prev_load = true;
 		}
 
 		if (load > max_load)
@@ -406,7 +411,6 @@ int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 				(j_cdbs->prev_cpu_wall - j_cdbs->prev_cpu_idle);
 			j_cdbs->prev_load = 100 * prev_load /
 					(unsigned int) j_cdbs->prev_cpu_wall;
-			j_cdbs->copy_prev_load = true;
 
 			if (ignore_nice)
 				j_cdbs->prev_cpu_nice =
