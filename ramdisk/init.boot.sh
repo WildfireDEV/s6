7524a492f459f09cfc4e66e90a02a66d34e51eb0
Park Ju Hyung <qkrwngud825@gmail.com>
2016-05-17 02:50:03 +0900

    ramdisk: limit cpus after bootanim has stopped
    Prior to this change, processes are limited to each cpuset rules,
    slowing down the boot sequence noticeably.
    
    Workaround this by setting cpus after bootanim svc has stopped.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- ramdisk/init.boot.sh
+++ ramdisk/init.boot.sh
@@ -8,7 +8,6 @@ if [[ "$(cat /data/.arter97/invisible_cpuset)" == "1" ]]; then
 	chown system:system /dev/cpuset/invisible/tasks
 	chmod 0755 /dev/cpuset/invisible
 	chmod 0644 /dev/cpuset/invisible/tasks
-	echo "0" > /dev/cpuset/invisible/cpus
+	echo "0-7" > /dev/cpuset/invisible/cpus
 	echo "0" > /dev/cpuset/invisible/mems
-	echo "0-3" > /dev/cpuset/invisible/cpus
 fi
