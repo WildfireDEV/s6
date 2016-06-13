525c79d1ef6c79e2de3e513ab8594848e1f4f73a
WildfireDEV <wildfiredev@gmail.com>
2016-06-13 11:45:50 +1000

    Revert "thermal: IPA: update power tables on actor voltage change"
    This reverts commit 7d642443964f6fc0220f0db6d7e87e177db6eddc.

--- drivers/cpufreq/exynos-mp-cpufreq.c
+++ drivers/cpufreq/exynos-mp-cpufreq.c
@@ -26,7 +26,6 @@
 #include <linux/reboot.h>
 #include <linux/delay.h>
 #include <linux/cpu.h>
-#include <linux/ipa.h>
 #include <linux/pm_qos.h>
 #include <linux/kobject.h>
 #include <linux/sysfs.h>
@@ -1428,8 +1427,6 @@ static ssize_t store_volt_table(struct kobject *kobj, struct attribute *attr,
 			exynos_info[cluster]->volt_table[i + invalid_offset] = t[i];
 		}
 	}
-
-	ipa_update();
 
 	mutex_unlock(&cpufreq_lock);
 
